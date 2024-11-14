ed99abbcba867fa3009650c76966327f9a02a71a
Mooncat <notmoondev@gmail.com>
2024-11-14 09:53:20 +0100

    fix: add Token to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,12 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f https://github.com/Moouncat/wordle-web.git master:gh-pages
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  exit 1
+fi
+
+git remote add origin https://x-access-token:${GITHUB_TOKEN}@github.com/Moouncat/wordle-web.git
+git push -f origin master:gh-pages
 
 cd -
